6535823af6ef787d3bc8f44724b5a21bbafd298f
ppwatchara <pee.watchara2019@gmail.com>
2021-02-24 12:18:07 +0700

    .

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -24,6 +24,27 @@ export class ChatComponent implements OnInit {
   messages: any = [];
   message: string;
 
+  bodydata = [
+    {
+      "createby": "Theerasak Tubrit",
+      "_id": "601180e128d90332b8ee5535",
+      "contactName": "watcharapong",
+      "contactImg": "https://scontent.fbkk13-2.fna.fbcdn.net/v/t1.0-9/134913096_3530000260440242_7114700459074630105_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeFelJSEvEL715OyqHxVuF9qe62trROoDGV7ra2tE6gMZQ3zqRGd4FoQaVvM7yMVdAoE6epsYKTydGplRLVUCmyh&_nc_ohc=791vf3mzxFAAX_NypbP&_nc_ht=scontent.fbkk13-2.fna&oh=0daafb52ffaf727923e529b171aa9ba1&oe=60410BF6",
+      "contactCompany": "บริษัท ละมุนภัณฑ์ เทรดดิ้ง จำกัด",
+      "contactPhoneNo": "0623235361",
+      "contactEmail": "pee.ch2014@gmail.com",
+      "contactLineId": "ppwatchara",
+      "contactAddress": "65-1",
+      "chat": "สวัสดี",
+      "contactChat":[],
+      "chatDate": "1/12/2020",
+      "created": "2021-01-27T15:04:01.496Z",
+      "__v": 0,
+      "id": "601180e128d90332b8ee5535",
+      "updateby": ""
+    }
+  ]
+
   constructor(private httpClient: HttpClient, private formBaseService: FormbaseService, public ChatService: ChatService, private SseService: SseService) { }
 
   ngOnInit(): void {
@@ -32,18 +53,19 @@ export class ChatComponent implements OnInit {
 
     this.ChatService.getServerEventSource('http://localhost:3000/events')
       .subscribe((chat) => {
-        console.log(JSON.parse(chat.data));
+        // console.log(JSON.parse(chat.data));
         let data = JSON.parse(chat.data);
-        console.log(data);
-        this.messages = data;
-        console.log(this.messages)
-
-
-        // if (data.type === 'qrSes') {
-        //   console.log(data);
-        //   this.qr = `/qrlogin/v1/qr/${data.value}`
-        //   console.log(this.qr);
-        // }
+        // console.log(data);
+        // this.messages = data;
+        // console.log(this.messages)
+        if(data.lenght > 0){
+          data.forEach(chatSend => {
+            this.bodydata[0].contactChat.push(chatSend);
+          });
+          this.messages = this.bodydata[0].contactChat;
+        }else{
+          this.messages.push(data);
+        }
       });
 
   }
@@ -61,11 +83,12 @@ export class ChatComponent implements OnInit {
     console.log(this.message);
 
     this.ChatService.sendData(body).then(() => {
-      this.ChatService.getServerEventSource('http://localhost:3000/events')
-        .subscribe((chat) => {
-          let data = JSON.parse(chat.data);
-          this.messages = data;
-        });
+      this.message ='';
+      // this.ChatService.getServerEventSource('http://localhost:3000/events')
+      //   .subscribe((chat) => {
+      //     let data = JSON.parse(chat.data);
+      //     this.messages = data;
+      //   });
     })
   }
 
